6d7be219673734a18e85f04cbfd407f78d5b1eb2
Julie Nuguet <julie@nuguet.fr>
2013-05-23 11:40:23 +0200

    mise à jour du code

---
+++ public/error.php
@@ -0,0 +1,18 @@
+<?php
+header("HTTP/1.1 500 INTERNAL SERVER ERROR");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>DEV-FLY - Erreur</title>
+    </head>
+    <body>
+        <h1>Erreur</h1>
+        <p>La page que vous cherchez n'a pas pu s'afficher correctement !<br>
+        Nous vous invitons à réessayer.</p>
+        
+        <p>Si l'erreur persiste, merci de contacter l'administrateur du site.</p>
+        
+    </body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+include '../setup.php';
+// on récupère le nom du controlleur saisi dans l'URL
+$controllerName = $_GET['controller'];
+// on récupèrera la méthode
+$methodName = $_GET['method'];
+
+$controllerClassName = "controller\\$controllerName";
+
+try{
+    // Code générique : quand on arrive sur l'index, on sait qu'on va appeler
+    // un controller et une méthode, mais on ne sait pas encore lesquels.
+    $class = new ReflectionClass($controllerClassName);
+    $instance = $class->newInstance();
+    $method = $class->getMethod($methodName);
+    $method->invoke($instance);
+} catch (Exception $ex){
+    include VIEW . 'error.php';
+}
+?>
