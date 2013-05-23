3c84fe2381c7e021da7d702ab542fcbebb2425ee
Julie Nuguet <julie@nuguet.fr>
2013-05-23 14:40:32 +0200

    mise à jour dao

--- dao/MysqlDao.php
+++ dao/MysqlDao.php
@@ -3,4 +3,14 @@
 namespace dao;
 
 use \PDO;
+
+class MysqlDao {
+
+    private $dbh;
+
+    public function __construct() {
+        $this->dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+        $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+}
 ?>
