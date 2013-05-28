b4b6bece5113b08212d3c7f313dad7516b56b2a6
Julie Nuguet <julie@nuguet.fr>
2013-05-28 12:25:09 +0200

    mise à jour MysqlDao pour affichage utf8 correct

--- dao/MysqlDao.php
+++ dao/MysqlDao.php
@@ -9,8 +9,11 @@ class MysqlDao {
     private $dbh;
 
     public function __construct() {
-        $this->dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
-        $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Connexion à la BD. Les options permettent de renvoyer une exception
+        // + d'afficher correctement en UTF8 :
+        $attributes = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'");
+        $this->dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD, $attributes);
     }
 }
 ?>
