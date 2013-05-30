c14d6d1c87532c11921d025c6a3c258a7c15eac3
greta <greta@greta-03>
2013-05-30 14:21:48 +0200

    page recherche

---
+++ public/recherche.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <title>DEV-FLY - Recherche</title>
+    </head>
+    <body>
+       <div id="supercontainer">
+          <header>
+            
+           
+          </header>
+            <div id="container">
+                 <form action="#" method="POST" id="form">
+            <fieldset>
+                <legend>Appartement</legend> 
+                <label>Nom</label>
+                <input 
+            </fieldset>
+            
+        </form>
+                
+            </div>
+            <footer>
+                
+            </footer>
+       </div>
+    </body>
+    
+</html>
