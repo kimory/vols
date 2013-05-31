608d16781c03ba3ed3e54f85eb3781ed446105b2
Julie Nuguet <julie@nuguet.fr>
2013-05-31 22:47:34 +0200

    traitement cas d'un identifiant non saisi

--- controller/affichageClientController.php
+++ controller/affichageClientController.php
@@ -5,9 +5,17 @@ use dao\MysqlDao;
 
 class affichageClientController{
     public function action(){
-        $dao = new MysqlDao();
-        $client = $dao->getInfosClientById($_POST['numclient']);
+        $message = null;
+        if(isset($_POST['numclient']) && strlen($_POST['numclient']) != 0){
+            $dao = new MysqlDao();
+            $client = $dao->getInfosClientById($_POST['numclient']);
+        }else {
+            $message ='Vous devez saisir l\'identifiant d\'un client pour
+                    obtenir sa description !';
+        }
         include VIEW."displayClient.php";
     }
+    
+    
 }
 ?>
--- public/displayClient.php
+++ public/displayClient.php
@@ -14,21 +14,27 @@
             <input type="submit" value="OK">
         </form>
         
-        <h3>Description du client</h3>
+        <?php if (isset($message) && strlen($message) > 0) : ?>
+                <p><?php echo $message ?></p>
+        <?php else : ?>
         
-        <!-- Remarque : le htmlentities est une sécurité, il convertit les caractères
-        spéciaux en entités HTML -->
-        <p>N° client : <?php echo htmlentities($client->getId(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Civilité : <?php echo htmlentities($client->getCivilite(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Nom : <?php echo htmlentities($client->getNom(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Prénom : <?php echo htmlentities($client->getPrenom(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Adresse : <?php echo htmlentities($client->getAdresse(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Code postal : <?php echo htmlentities($client->getCp(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Ville : <?php echo htmlentities($client->getVille(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Pays : <?php echo htmlentities($client->getPays(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>E-mail : <?php echo htmlentities($client->getMail(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Tél fixe : <?php echo htmlentities($client->getTelFixe(), ENT_QUOTES, 'UTF-8') ?></p>
-        <p>Tél portable : <?php echo htmlentities($client->getTelPortable(), ENT_QUOTES, 'UTF-8') ?></p>
+            <h3>Description du client</h3>
+
+            <!-- Remarque : le htmlentities est une sécurité, il convertit les caractères
+            spéciaux en entités HTML -->
+            <p>N° client : <?php echo htmlentities($client->getId(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Civilité : <?php echo htmlentities($client->getCivilite(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Nom : <?php echo htmlentities($client->getNom(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Prénom : <?php echo htmlentities($client->getPrenom(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Adresse : <?php echo htmlentities($client->getAdresse(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Code postal : <?php echo htmlentities($client->getCp(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Ville : <?php echo htmlentities($client->getVille(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Pays : <?php echo htmlentities($client->getPays(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>E-mail : <?php echo htmlentities($client->getMail(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Tél fixe : <?php echo htmlentities($client->getTelFixe(), ENT_QUOTES, 'UTF-8') ?></p>
+            <p>Tél portable : <?php echo htmlentities($client->getTelPortable(), ENT_QUOTES, 'UTF-8') ?></p>
+        
+        <?php endif; ?>
                 
         <p><a href="/choixducritere">retour</a></p>
         
