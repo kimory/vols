9e0c13d52a32bf6c3a502c8b771afbf5eebb7f45
greta <greta@greta-03>
2013-06-06 16:24:33 +0200

    test mysqldao

--- dao/MysqlDao.php
+++ dao/MysqlDao.php
@@ -42,7 +42,8 @@ class MysqlDao {
             }
         }
         return $tab;
-    }public function getPropositionsByVol($villedep, $villearrivee, $datedep){
+    }
+    public function getPropositionsByVol($villedep, $villearrivee){
         // recupere les infos saisies par le client et verifier les donnees saisies qui sont les ville de depart et ville d'arrivee et
         //si la compagnie propose la destination.
         $sql = "SELECT lieudep,lieuarriv,dateheuredep FROM vol            
@@ -56,9 +57,8 @@ class MysqlDao {
             $lieuDepart = $row['lieudep'];
             $lieuArrivee = $row['lieuarriv'];
             $dateHeureDepart = $row['dateheuredep'];
-               $result[] = new Vol(NULL, $lieuDepart, $lieuArrivee, $dateHeureDepart, NULL, NULL);
-                                      
-        }return $result; 
+            $result[] = new Vol(NULL, $lieuDepart, $lieuArrivee, $dateHeureDepart, NULL, NULL,NULL,NULL,NULL,NULL,NULL,NULL);                
+        }return $result;
     }
 
     
