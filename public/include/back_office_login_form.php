0e26fe0aa07eb8b4f179d66b803af422535737b9
Philippe Pittoli <philippe.pittoli@etu.unistra.fr>
2013-06-08 17:33:32 +0200

    affichage erreur 'message' quand erreur login / passwd

--- public/include/back_office_login_form.php
+++ public/include/back_office_login_form.php
@@ -1,9 +1,16 @@
 <form action="/backOfficeLoginController" method="POST" >
+<?php 
+if(isset($_SESSION['message']) && strlen($_SESSION['message']) > 0) {
+	echo $_SESSION['message'] . PHP_EOL;
+
+	$_SESSION['message'] = '';
+}
+?>
 	<label for ="login">login</label>
 		<input type="text" name="login" id="login">
 
 	<label for ="passwd">password</label>
-		<input type="password" name="passwd" id="passwd"><br>
+		<input type="password" name="passwd" id="passwd">
 
 	<input type="submit" name="valider" id="valider" value="ok">          
 </form>
