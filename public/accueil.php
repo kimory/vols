19cfadb906cbc2a2efab6efb069c9542a464efef
Julie Nuguet <julie@nuguet.fr>
2013-06-08 16:23:10 +0200

    encodage UTF8 sur la page d'accueil

--- public/accueil.php
+++ public/accueil.php
@@ -1,19 +1,15 @@
 <?php
   
 ?>
-
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
   <head>
-     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<meta http-equiv="content-language" content="fr">
-<title>DEV-FLY - Accueil</title>
-     <link rel="stylesheet" type="text/css" href="css/style.css" />
-
-</head>
-    <body>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>DEV-FLY - Accueil</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+  </head>
+    
+  <body>
 	
 	  <p><a href="/recherche">Français</a> | <a href=#>Anglais</a></p>
 	   	   
