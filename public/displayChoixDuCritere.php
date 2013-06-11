33fb1015530c98da7b6a09494921f465c6e865a9
Julie Nuguet <julie@nuguet.fr>
2013-06-11 19:51:14 +0200

    todo + vérif avant d'arriver au choix du critère

--- public/displayChoixDuCritere.php
+++ public/displayChoixDuCritere.php
@@ -2,6 +2,15 @@
 if (!isset($_SESSION)) {
     session_start();
 }
+include_once("../setup.php");
+
+use entity\User;
+
+if(!User::isAdminConnected()){
+    // Si un utilisateur non connecté en tant qu'admin tente d'accéder à cette page directement
+    // On force son passage vers le contrôleur qui fait les vérifications nécessaires :
+    header("Location:/backOfficeController");
+}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
