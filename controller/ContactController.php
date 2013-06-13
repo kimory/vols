02fc6be571a84169f5fcf860c57c1c867a4a988f
greta <greta@greta-03>
2013-06-13 12:31:18 +0200

    modification des conditions du contactcontroller

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -15,19 +15,22 @@ class ContactController {
         // On vérifie que les champs ont été correctement renseignés
         // htmlentities permet de convertir tous les caractères éligibles en entités HTML  
         // ENT_QUOTES permet de convertir les doubles quotes et simples quotes en entités HTML 
-        if(isset($_POST['nom']) && strlen($_POST['nom']) > 0) {
+        if(!isset($_POST['nom']) || strlen($_POST['nom']) == 0 ){            
+            $messages[] = "Merci d'indiquer votre nom.";            
+        }elseif(ctype_digit($_POST['nom'])){
+            $messages[] = "Votre saisie est incorrecte";
+        }else{
             $nom = htmlentities($_POST['nom'], ENT_QUOTES, 'UTF-8');
-        } else {
-            $messages[] = "Merci d'indiquer votre nom.";
-        }
-        
-        if(isset($_POST['prenom']) && strlen($_POST['prenom']) > 0) {
-            $prenom = htmlentities($_POST['prenom'], ENT_QUOTES, 'UTF-8');
-        } else {
+        }       
+             
+        if(!isset($_POST['prenom']) || strlen($_POST['prenom']) == 0 ){
             $messages[] = "Merci d'indiquer votre prénom.";
+        }elseif(ctype_digit($_POST['prenom'])){
+            $messages[] = "Votre saisie est incorrecte";
+        } else {            
+            $prenom = htmlentities($_POST['prenom'], ENT_QUOTES, 'UTF-8');           
         }
-        // verifier aussi le caractere speciale de l'adresse mail
-        
+                
         if(isset($_POST['mail']) && strlen($_POST['mail']) > 0 && preg_match("/^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/",$_POST['mail'])){
             $mail = htmlentities($_POST['mail'], ENT_QUOTES, 'UTF-8');
         } else {
@@ -38,19 +41,15 @@ class ContactController {
             $sujet = htmlentities($_POST['sujet'], ENT_QUOTES, 'UTF-8');
         } else {
             $messages[] = "Merci d'indiquer votre sujet.";
-        }
-        // verifier si le caractere saisie est bien celui d'un tel et qui peut etre verifier si c'est un numero internationale 
-
-
+        } 
         
         if(isset($_POST['tel']) && preg_match("/^[0-9]{10,20}$/",$_POST['tel'])){
             $telephone = htmlentities($_POST['tel'], ENT_QUOTES, 'UTF-8');
 
-
         } else {
             $messages[] = "Merci d'indiquer votre téléphone.";
         }
-        // verifie si le respecte les bons caracteres d'un mail.        
+                
         if(isset($_POST['message']) && strlen($_POST['message']) > 0 ) {
             $message = htmlentities($_POST['message'], ENT_QUOTES, 'UTF-8');
         } else {
