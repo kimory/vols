03385ad1cd361fc51791e892df102a20a46b36b4
Julie Nuguet <julie@nuguet.fr>
2013-06-13 09:58:08 +0200

    suppression ligne inutile

--- public/espaceClient.php
+++ public/espaceClient.php
@@ -6,7 +6,6 @@ if (!isset($_SESSION)) {
 include_once("../setup.php");
 
 use entity\Client;
-use entity\User;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
