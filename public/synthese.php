499fd143ebca6ce8b06e5b393ae661f0e6b10c03
Julie NUGUET <julie@nuguet.fr>
2013-06-14 23:52:43 +0200

    Synthèse quasi-fini, il manque le controleur

--- public/synthese.php
+++ public/synthese.php
@@ -53,6 +53,44 @@ use entity\Passager;
 ?>
 		</div>
 		<div class="container">
+		<p>Les e-cartes bleues et cartes transcash ne sont pas acceptées.</p>
+		<form action="/syntheseController" >
+			<label for="numcarte">Numéro de carte</label>
+			<input type="text" name="numcarte" id="numcarte">
+			
+			<label for="moisexpiration">Date d'expiration</label>
+			<select name='moisexpiration' id='moisexpiration' >
+				<option value="1">01</option>
+				<option value="2">02</option>
+				<option value="3">03</option>
+				<option value="4">04</option>
+				<option value="5">05</option>
+				<option value="6">06</option>
+				<option value="7">07</option>
+				<option value="8">08</option>
+				<option value="9">09</option>
+				<option value="10">10</option>
+				<option value="11">11</option>
+				<option value="12">12</option>
+			</select>
+			<select name='anneeexpiration' id='anneeexpiration' >
+				<option value="13">13</option>
+				<option value="14">14</option>
+				<option value="15">15</option>
+				<option value="16">16</option>
+				<option value="17">17</option>
+				<option value="18">18</option>
+			</select>
+			<label for="nomporteur">Nom du porteur</label>
+			<input type="text" name="nomporteur" id="nomporteur">
+
+			<label for="codesecurite">Code de sécurité</label>
+			<input type="text" name="codesecurite" id="codesecurite">
+			<input type="reset" value="annuler">
+			<input type="submit" value="Valider">
+		</form>
+		</div>
+		<div class="container">
 			<div id="footer">
 				<p> &nbsp;&nbsp; &copy; Tous droits réservés &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-- DEV-FLY 2013 --</p>
 			</div> 
