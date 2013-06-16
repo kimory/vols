7cc92e5ac12a3295d77bc9e11ad5565fdd01911b
Bijou <Bijou@192.168.1.36>
2013-06-16 19:57:36 +0200

    passager

---
+++ controller/createPassagerController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace controller;
+
+use dao\MysqlDao;
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+class CreatePassagerController {
+
+    public function inscription() {
+
+        $messages = array(); // On initialise un tableau d'erreurs potentielles
+        // On vérifie que les champs ont été correctement renseignés
+        if(!isset($_POST['nom']) || strlen($_POST['nom']) == 0 ){            
+            $messages[] = "Merci d'indiquer votre nom.";            
+        }elseif(ctype_digit($_POST['nom'])){
+            $messages[] = "Le nom saisi est incorrecte.";
+        }else{
+            $nom = htmlentities($_POST['nom'], ENT_QUOTES, 'UTF-8');
+        }       
+             
+        if(!isset($_POST['prenom']) || strlen($_POST['prenom']) == 0 ){
+            $messages[] = "Merci d'indiquer votre prénom.";
+        }elseif(ctype_digit($_POST['prenom'])){
+            $messages[] = "Le prénom saisi est incorrecte.";
+        } else {            
+            $prenom = htmlentities($_POST['prenom'], ENT_QUOTES, 'UTF-8');           
+        }
+         if (isset($_POST['jour']) && ctype_digit($_POST['jour']) && $_POST['jour'] > 0 && $_POST['jour'] <= 31) {
+            $jour = trim($_POST['jour']);
+        } else {
+            $messages[] = "Le jour est incorrect.";
+        }
+
+        if (isset($_POST['mois']) && ctype_digit($_POST['mois']) && $_POST['mois'] > 0 && $_POST['mois'] <= 12) {
+            $mois = trim($_POST['mois']);
+        } else {
+            $messages[] = "Le mois est incorrect.";
+        }
+
+        if (isset($_POST['annee']) && ctype_digit($_POST['annee'])) {
+            $mois = trim($_POST['mois']);
+        } else {
+            $messages[] = "L'année mois est incorrect.";
+        }
+           
+       if(empty($messages)){
+            //header('location:/displaycontact');   
+            include VIEW . "#";;
+        }else{
+            $_SESSION['messages'] = $messages;
+            header('Location:' . $_SERVER['HTTP_REFERER']); // renvoie vers la page précédent          
+        }
+        
+        }
+    }
+
+
+
+?>
