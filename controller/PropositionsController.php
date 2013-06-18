60673caad0286d33ad95966ab2106fd3bf952ded
Julie Nuguet <julie@nuguet.fr>
2013-06-18 23:29:47 +0200

    remaniement formulaire recherche

--- controller/PropositionsController.php
+++ controller/PropositionsController.php
@@ -10,48 +10,78 @@ use \DateTime;
 class PropositionsController {
 
     public function action() {
-
+        
         $messages = array(); // On initialise un tableau d'erreurs potentielles
         // On vérifie que les champs ont été correctement renseignés
-        if (isset($_POST['villedepart']) && strlen($_POST['villedepart']) > 0 &&
-                preg_match("/^[A-Za-zàâäéèêëìîïôöòùûüçÀÂÄÉÈËÏÎÌÔÖÙÛÜÇ-]+$/", $_POST['villedepart'])) {
-            $villedepart = htmlentities($_POST['villedepart'], ENT_QUOTES, 'UTF-8');
+        if (isset($_POST['villedepart']) && strlen($_POST['villedepart']) > 0) {
+            if (preg_match("/^[A-Za-zàâäéèêëìîïôöòùûüçÀÂÄÉÈËÏÎÌÔÖÙÛÜÇ-]+$/", $_POST['villedepart'])) {
+                $villedepart = htmlentities($_POST['villedepart'], ENT_QUOTES, 'UTF-8');
+            } else {
+                // On stocke la valeur saisie pour éviter à l'utilisateur une ressaisie
+                $_SESSION['villedepart'] = $_POST['villedepart'];
+                $messages[] = "La ville de départ est incorrecte.";
+            }
         } else {
-            $messages[] = "La ville de départ est incorrecte.";
+            $messages[] = "Veuillez renseigner une ville de départ.";
         }
 
-        if (isset($_POST['villearrivee']) && strlen($_POST['villearrivee']) > 0 &&
-            preg_match("/^[A-Za-zàâäéèêëìîïôöòùûüçÀÂÄÉÈËÏÎÌÔÖÙÛÜÇ-]+$/", $_POST['villearrivee'])) {
-            $villearrivee = htmlentities($_POST['villearrivee'], ENT_QUOTES, 'UTF-8');
+        if (isset($_POST['villearrivee']) && strlen($_POST['villearrivee']) > 0) {
+            if (preg_match("/^[A-Za-zàâäéèêëìîïôöòùûüçÀÂÄÉÈËÏÎÌÔÖÙÛÜÇ-]+$/", $_POST['villearrivee'])) {
+                $villearrivee = htmlentities($_POST['villearrivee'], ENT_QUOTES, 'UTF-8');
+            } else {
+                // On stocke la valeur saisie pour éviter à l'utilisateur une ressaisie
+                $_SESSION['villearrivee'] = $_POST['villearrivee'];
+                $messages[] = "La ville d'arrivée est incorrecte.";              
+            }
         } else {
-            $messages[] = "La ville d'arrivée est incorrecte.";
+            $messages[] = "Veuillez renseigner une ville d'arrivée.";
         }
         
         // On vérifie que les villes de départ et d'arrivée saisies ne sont pas les mêmes :
         if (isset($_POST['villedepart']) && isset($_POST['villearrivee']) &&
             $_POST['villedepart'] == $_POST['villearrivee']){
+            // On stocke la valeur saisie pour éviter à l'utilisateur une ressaisie
+            $_SESSION['villedepart'] = $_POST['villedepart'];
+            $_SESSION['villearrivee'] = $_POST['villearrivee'];
             $messages[] = "Les villes de départ et d'arrivée doivent être différentes.";
         }        
 
-        if (isset($_POST['jour']) && ctype_digit($_POST['jour']) && $_POST['jour'] > 0 && $_POST['jour'] < 32) {
-            $jour = trim($_POST['jour']);
+        if (isset($_POST['jour'])) {
+            if (ctype_digit($_POST['jour']) && $_POST['jour'] > 0 && $_POST['jour'] < 32) {
+                $jour = trim($_POST['jour']);
+            } else {
+                // On stocke la valeur saisie pour éviter à l'utilisateur une ressaisie
+                $_SESSION['jour'] = $_POST['jour'];
+                $messages[] = "Le jour est incorrect.";
+            }
         } else {
-            $messages[] = "Le jour est incorrect.";
+            $messages[] = "Veuillez indiquer le jour de départ souhaité.";
         }
 
-        if (isset($_POST['mois']) && ctype_digit($_POST['mois']) && $_POST['mois'] > 0 && $_POST['mois'] < 13) {
-            $mois = trim($_POST['mois']);
+        if (isset($_POST['mois'])) {
+            if (ctype_digit($_POST['mois']) && $_POST['mois'] > 0 && $_POST['mois'] < 13) {
+                $mois = trim($_POST['mois']);
+            } else {
+                // On stocke la valeur saisie pour éviter à l'utilisateur une ressaisie
+                $_SESSION['mois'] = $_POST['mois'];
+                $messages[] = "Le mois est incorrect.";
+            }
         } else {
-            $messages[] = "Le mois est incorrect.";
+            $messages[] = "Veuillez indiquer le mois de départ souhaité.";
         }
 
         $datedujour = new DateTime(); // On récupère la date du jour
         $anneeCourante = $datedujour->format("Y"); // On récupère l'année courante
-        if (isset($_POST['annee']) && ctype_digit($_POST['annee']) &&
-                ($_POST['annee'] == $anneeCourante || $_POST['annee'] == $anneeCourante + 1)) {
-            $annee = trim($_POST['annee']);
+        if (isset($_POST['annee'])) {
+            if (ctype_digit($_POST['annee']) && ($_POST['annee'] == $anneeCourante || $_POST['annee'] == $anneeCourante + 1)) {
+                $annee = trim($_POST['annee']);
+            } else {
+                // On stocke la valeur saisie pour éviter à l'utilisateur une ressaisie
+                $_SESSION['annee'] = $_POST['annee'];
+                $messages[] = "L'année est incorrecte.";
+            }
         } else {
-            $messages[] = "L'année est incorrecte";
+            $messages[] = "Veuillez indiquer l'année de départ souhaitée.";
         }
 
         if (isset($jour) && isset($mois) && isset($annee)) {
@@ -66,6 +96,10 @@ class PropositionsController {
             $dt = DateTime::createFromFormat('d/m/Y', $date);
             if ($dt < $datedujour) {
                 $messages[] = "La date de départ ne peut pas être antérieure à la date du jour.";
+                // On stocke la valeur saisie pour éviter à l'utilisateur une ressaisie
+                $_SESSION['jour'] = $jour;
+                $_SESSION['mois'] = $mois;
+                $_SESSION['annee'] = $annee;
             } else {
                 $datedepart = $dt;
             }
@@ -95,6 +129,11 @@ class PropositionsController {
                 // Cas où aucun vol ne correspond à la demande du client
                 $messages[] = "Navrés ! Aucun vol ne correspond à votre demande. Vous pouvez effectuer une nouvelle recherche.";
                 $_SESSION['messages'] = $messages;
+                $_SESSION['villedepart'] = $_POST['villedepart'];
+                $_SESSION['villearrivee'] = $_POST['villearrivee'];
+                $_SESSION['jour'] = $_POST['jour'];
+                $_SESSION['mois'] = $_POST['mois'];
+                $_SESSION['annee'] = $_POST['annee'];
                 header('Location:/recherche');
             } else {
                 // Je stocke en session les éléments à conserver et j'envoie vers la vue Proposition
--- public/recherche.php
+++ public/recherche.php
@@ -68,32 +68,61 @@ if (!isset($_SESSION)) {
                         <h5>Votre sélection</h5>
 
                         <label for ="villedepart">Ville de départ</label>
-                           <input type="text" name="villedepart" id="villedepart">
+                           <input type="text" name="villedepart" id="villedepart" value="<?php 
+				if(isset($_SESSION['villedepart']))
+				{
+					echo $_SESSION['villedepart'];
+                                        unset($_SESSION['villedepart']);
+				}?>">
 
                         <label for ="villearrivee">Ville d'arrivée</label>
-                           <input type="text" name="villearrivee" id="villearrivee"><br>
-                                   <div id="date">
+                           <input type="text" name="villearrivee" id="villearrivee" value="<?php 
+				if(isset($_SESSION['villearrivee']))
+				{
+					echo $_SESSION['villearrivee'];
+                                        unset($_SESSION['villearrivee']);
+				}?>">                                
+                                  <br>
+                        <div id="date">
                         <label for ="datedepart">Date de départ (jj/mm/aaaa) </label>                        
-                           <input type="text" id="jour" name="jour" >/
-                           <input type="text" id="mois" name="mois" >/
-                           <input type="text" id="annee" name="annee" ><br>
+                           <input type="text" id="jour" name="jour" value="<?php 
+				if(isset($_SESSION['jour']))
+				{
+					echo $_SESSION['jour'];
+                                        unset($_SESSION['jour']);
+				}?>">                                      
+                            /
+                           <input type="text" id="mois" name="mois" value="<?php 
+				if(isset($_SESSION['mois']))
+				{
+					echo $_SESSION['mois'];
+                                        unset($_SESSION['mois']);
+				}?>">        
+                            /
+                           <input type="text" id="annee" name="annee" value="<?php 
+				if(isset($_SESSION['annee']))
+				{
+					echo $_SESSION['annee'];
+                                        unset($_SESSION['annee']);
+				}?>">
+                               <br>
+                        </div>
                                 
                         <label for ="nbreadultes">Nombre d'adultes</label>
                            <input type='number' value="1" min='1' max='50' name="nbreadultes" id="nbreadultes">
 
                         <label for ="nbreenfants">Nombre d'enfants (moins de 3 ans)</label>
                            <input type='number' value="0" min='0' max='50' name="nbreenfants" id="nbreenfants"><br>
-                             </div>
+                       
                         
                            <input type="submit" value="valider">          
                            <input type="reset" value="annuler">
                  
-
                     </fieldset>
                   </div>
                 </form>
-
              </div>
+            
             <div id="footer">
                <?php
                 include './include/footer.php';
