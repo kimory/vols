3607ba945a121355e762762118cffbb09bd6e14d
greta <greta@greta-03>
2013-06-18 16:42:01 +0200

    ajout de la method addcontact + modif du contact controller au nivo du " sujet".

--- controller/ContactController.php
+++ controller/ContactController.php
@@ -34,12 +34,12 @@ class ContactController {
             $messages[] = "Votre saisie du mail est  incorrecte.";
         }
         
-        if(isset($_POST['sujet']) && strlen($_POST['sujet']) > 0) {
+        if(!isset($_POST['sujet']) || strlen($_POST['sujet']) == 0) {
+            $messages [] = "Merci d'indiquer votre sujet.";
+        }elseif(!preg_match("/^[a-zA-Z1-9àáâãäåçèéêëìíîïðòóôõöùúûüýÿ-]$/", $_POST['sujet'])){            
+             $messages[] = "Votre saisie du sujet est incorrecte.";
+        }else{           
             $sujet = htmlentities($_POST['sujet'], ENT_QUOTES, 'UTF-8');
-        }elseif(!preg_match("/^[a-zA-Z1-9àáâãäåçèéêëìíîïðòóôõöùúûüýÿ-]$/", $_POST['sujet'])){
-            $messages [] = "Votre saisie du sujet est incorrecte";
-        }else{
-            $messages[] = "Votre saisie du sujet est incorrecte.";
         } 
         
         if(isset($_POST['tel']) && preg_match("/^[0-9]{10,20}$/",$_POST['tel'])){
