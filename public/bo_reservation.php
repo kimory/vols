df6bd76c805beee5507ba7b508256ab39c1205f6
Julie Nuguet <julie@nuguet.fr>
2013-07-22 16:09:24 +0200

    petites améliorations

--- public/bo_reservation.php
+++ public/bo_reservation.php
@@ -73,7 +73,7 @@ if (!isset($_SESSION)) {
                         <p>Date de départ : <?php echo htmlentities($dateheuredep->format('d/m/Y'), ENT_QUOTES, 'UTF-8') ?></p>
                         <p>Heure de départ : <?php echo htmlentities($dateheuredep->format('H:i'), ENT_QUOTES, 'UTF-8') ?></p>
                         <!-- On veut récupérer le numéro du client pour l'utiliser dans la méthode action
-                        du controller AffichageClientC : -->
+                        du controller AffichageClientController : -->
                         <p>N° client : <a href="/client/<?php echo htmlentities($reservation->getClient()->getId(), ENT_QUOTES, 'UTF-8') ?>"><?php echo htmlentities($reservation->getClient()->getId(), ENT_QUOTES, 'UTF-8') ?></a></p>
                         <p>Nombre de passager(s) : <a href="/passagers/<?php echo htmlentities($reservation->getNumReservation(), ENT_QUOTES, 'UTF-8') ?>"><?php echo htmlentities($reservation->getNbpassagers(), ENT_QUOTES, 'UTF-8') ?></a></p>
                     </div>
